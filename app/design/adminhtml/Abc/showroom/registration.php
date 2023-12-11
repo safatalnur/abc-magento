0e2c71706d114cb381828ed22e53c07cd13ea115
Safat Alnur <salnur@lenbrook.com>
2023-12-11 08:31:21 -0500

    CHORES: create custom admin theme, change admin logo

---
+++ app/design/adminhtml/Abc/showroom/registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Copyright &copy; Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'adminhtml/Abc/showroom', __DIR__); // Example: 'adminhtml/Magento/backend'
